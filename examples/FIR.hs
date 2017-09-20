99e6ff006a03e1b3e020465a1e7e5c49885338b5
Austin Seipp <aseipp@pobox.com>
2017-09-20 16:33:57 -0500

    Initial 1.0 documentation
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

---
+++ examples/FIR.hs
@@ -0,0 +1,8 @@
+import Clash.Prelude
+
+-- The dot product: a summation of products between two vectors
+dotp xs ys = sum (zipWith (*) xs ys)
+
+-- The FIR: a dot product of coefficient signals against a sliding window of
+-- the input
+fir coeffs x = dotp coeffs (window x)
